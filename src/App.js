070dd1e21c91418dbaac6e563dcccd1fa74dbf71
[angoor2002] <angoorcodes@gmail.com>
2023-02-15 12:28:20 +0530

    app made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [val, setVal] = useState(0);
+  let increment = () => {
+    setVal(val + 1);
+  };
+  let decrement = () => {
+    setVal(val - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <p>{val}</p>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
     </div>
   );
 }
